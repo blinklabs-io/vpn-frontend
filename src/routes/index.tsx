c257563c016252aedd00be84e7a385b1019c21cc
JaeBrian <bnlee419@gmail.com>
2025-08-18 20:36:47 -0700

    add temporary privacy policy page and how it works
    Signed-off-by: JaeBrian <bnlee419@gmail.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router'
 import Home from '../pages/Home'
 import Account from '../pages/Account'
+import PrivacyPolicy from '../pages/PrivacyPolicy'
+import HowItWorks from '../pages/HowItWorks'
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/account" element={<Account />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/how-it-works" element={<HowItWorks />} />
     </Routes>
   )
 }
